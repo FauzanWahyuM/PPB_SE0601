f0be176010b16766cdce0f54668c435d373800e9
Fauzan Wahyu Mubarak <114811887+FauzanWahyuM@users.noreply.github.com>
2024-09-26 11:56:52 +0700

    Update

---
+++ Pertemuan_02/main.dart
@@ -0,0 +1,12 @@
+void main() {
+  int namamahasiswa = 10;
+  print(namamahasiswa);
+
+  bool IsMarried = true;
+  bool IsMan = false;
+  bool IsWomen = true;
+
+  int umur = 20;
+
+  double tinggiBadan = 178.6;
+}
