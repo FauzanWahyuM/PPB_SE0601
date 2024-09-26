8490cef7e11ec6c010961bff4bfecc45281a888a
Fauzan Wahyu Mubarak <114811887+FauzanWahyuM@users.noreply.github.com>
2024-09-26 13:02:13 +0700

    Update

--- Pertemuan_02/main.dart
+++ Pertemuan_02/main.dart
@@ -1,12 +1,44 @@
-void main() {
-  int namamahasiswa = 10;
-  print(namamahasiswa);
+// void main() {
+//   // int namamahasiswa = 10;
+//   // print(namamahasiswa);
 
-  bool IsMarried = true;
-  bool IsMan = false;
-  bool IsWomen = true;
+//   // String nama = "Fauzan";
+//   // print(nama);
 
-  int umur = 20;
+//   // bool IsMarried = true;
+//   // bool IsMan = false;
+//   // bool IsWomen = true;
 
-  double tinggiBadan = 178.6;
-}
+//   // int umur = 20;
+
+//   // double tinggiBadan = 178.6;
+
+//   // print(IsMarried);
+//   // print(IsMan);
+//   // print(IsWomen);
+//   // print(umur);
+//   // print(tinggiBadan);
+
+//   dynamic umur = 10;
+
+//   umur = "10";
+
+//   print(umur);
+
+//   List<dynamic> nama = [
+//     "Fauzan",
+//     "Gide",
+//     2,
+//     true,
+//     2.19,
+//     ["Game", "Coba"]
+//   ];
+//   print(nama[5][1]);
+// }
+
+// 1. buatkan algoritma untuk
+// - menghitung rata2 dari ipk
+// - menjumlahan berapa banyak mahasiswa yang sudah nikah
+// 2. masing2 dari point2 1a dan 1b diimplementasikan dalam kodingan
+
+void main() {}
